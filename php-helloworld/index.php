6c0c4a6def54ae02ef95d4a767c841294a52e7d1
Student User <student@workstation.lab.example.com>
2021-09-26 15:34:00 -0400

    another change to console1a

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Another change to console1 branch is coming.\n";
+print "Another change to console1 branch is coming.\n";
 ?>
